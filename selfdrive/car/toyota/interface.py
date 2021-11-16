f5ced34e735f5768a42814f4f6528200bb2d25bf
bobmeeks01 <58351215+bobmeeks01@users.noreply.github.com>
2021-11-16 16:44:09 -0700

    Bobs Turning Settings

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -101,11 +101,11 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = True
       ret.safetyConfigs[0].safetyParam = 73
       ret.wheelbase = 2.79
-      ret.steerRatio = 14.8
+      ret.steerRatio = 13.7
       tire_stiffness_factor = 0.5533  # not optimized yet
       ret.mass = 4387. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.05]]   # [[0.6], [0.1]]
+      ret.lateralTuning.pid.kf = .00006     # 0.00007818594
 
     elif candidate == CAR.LEXUS_RXH_TSS2:
       stop_and_go = True
